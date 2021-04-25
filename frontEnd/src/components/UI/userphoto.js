741e5e78e687f9e9b3ce00cf16110625880326d9
Mikołaj Strusiński <mikistrus@gmail.com>
2021-04-25 15:06:07 +0200

    [ADDED] user name is now shown under the user photo
    I have deleted the template name, and now the user name is shown when the user hovers over his photo

--- frontEnd/src/components/UI/userphoto.js
+++ frontEnd/src/components/UI/userphoto.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import logout from '../../logout';
 
@@ -6,13 +6,31 @@ import classes from './userphoto.module.css';
 import photo from '../../assets/userPhoto/image.jfif'
 
 const UserPhoto = (props) => {
+    let userData;
+    let getUserData = () => {
+        let local = localStorage.getItem('userData');
+        let session = sessionStorage.getItem('userData');
+        if(local !== null){
+            userData = JSON.parse(local);
+        }
+        else if(session !== null){
+            userData = JSON.parse(session);
+        }
+    }
+
     const [logOut, setlogOut] = useState(false);
+    const [nickname, setNickname] = useState();
+
+    useEffect(() => {
+        getUserData();
+        setNickname(userData.nickname);
+    });
 
     return ( 
         <div className={classes.dropdown}>
             <img alt="user" src={photo} className={classes.userPhoto}/>
             <div className={classes.dropdownContent}>
-                <h1 className={classes.dropdownUsername}>{props.username}</h1>
+                <h1 className={classes.dropdownUsername}>{nickname}</h1>
                 <hr />
                 <p>My Profile</p>
                 <p>Settings</p>
