43ee8ba3fb956b568ad9810ecab3f3a497d25c6c
Mikołaj Strusiński <mikistrus@gmail.com>
2021-04-29 10:28:43 +0200

    [ADDED] icons to menu bar

--- frontEnd/src/components/menu/menu.js
+++ frontEnd/src/components/menu/menu.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FaUserFriends } from 'react-icons/fa';
+import { IoNotifications, IoChatbubbles } from 'react-icons/io5';
 import classes from './menu.module.css';
 import Logo from '../UI/logo';
 import UserPhoto from '../UI/userphoto';
@@ -8,7 +10,12 @@ const menu = () => {
     return ( 
         <nav className={classes.Menu}>
             <Logo />
-            <UserPhoto username="Alice Wonderfell" />
+            <div className={classes.iconContainer}>
+                <FaUserFriends size="2em" color="#0a42a4"/>
+                <IoChatbubbles size="2em" color="#0a42a4"/>
+                <IoNotifications size="2em" color="#0a42a4"/>
+            </div>
+            <UserPhoto/>
         </nav>
     );
 }
