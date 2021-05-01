30c6474b95d5c89c894ad332d378d8ae51397df2
Mikołaj Strusiński <mikistrus@gmail.com>
2021-05-01 17:06:01 +0200

    [ADDED] blogs tab now gets the data about the blogs and displays them correctly

--- frontEnd/src/containers/userProfile/tabs/Blogs.js
+++ frontEnd/src/containers/userProfile/tabs/Blogs.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import getToken from '../../../getToken';
 import classes from './Blogs.module.css';
 
 class BlogsTab extends Component {
     constructor(props){
         super(props);
-        this.state = {
 
+        let token = getToken()
+
+        this.state = {
+            token: token,
+            blogs: [],
         }
-        this.getData.bind(this);
+        // this.getData.bind(this);
     }
 
     //get user Blogs and userData from userId from AJAX call 
-    getData () {
+    componentDidMount () {
         axios({
-            method: 'post',
-            url: `http://localhost:3001/`,
-            headers: {},
+            method: 'get',
+            url: `http://localhost:3001/blogs`,
+            headers: {'Authorization': this.state.token},
             data: {}
         })
         .then((res)=>{
             if(res.status===200){
-                
+                let blogs = [];
+                res.data.blogs.forEach(element => {
+                    blogs.push(element);
+                });
+                this.setState({blogs: blogs})
                 return;
             }
         })
@@ -35,7 +44,9 @@ class BlogsTab extends Component {
     render() { 
         return (
             <div>
-                hello
+                {this.state.blogs.map((el, index)=>(
+                    <div key={index}>{el.title}</div>
+                ))}
             </div>
         );
     }
