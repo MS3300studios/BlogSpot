bad5beb502ae27c1e9b1430d721fdf1d0529f6ac
Mikołaj Strusiński <mikistrus@gmail.com>
2021-07-11 00:15:04 +0200

    [ADDED] API functionality for photos, still some routes need to be added

--- models/photo.js
+++ models/photo.js
@@ -2,18 +2,24 @@ const db = require("../db");
 
 const likeSchema = new db.Schema({ authorId: {type: String}});
 const dislikeSchema = new db.Schema({ authorId: {type: String}});
+const commentSchema = new db.Schema({ authorId: {type: String}, content: {type: String}});
 
 const schema = new db.Schema({
     authorId: {type: String, require: true},
     description: {type: String},
     data: {type: String, require: true},
     likes: [likeSchema],
-    dislikes: [dislikeSchema]
+    dislikes: [dislikeSchema],
+    comments: [commentSchema]
   },
   {
     timestamps: true
   });
 
 const Photo = db.model("Photo", schema);
+const PhotoComment = db.model("PhotoComment", commentSchema);
 
-module.exports = Photo;
+module.exports = {
+    photo: Photo,
+    photoComment: PhotoComment
+}
---
+++ routes/photos.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const router = express();
+
+
+const auth = require('../middleware/authorization');
+
+const Module = require('../models/photo');
+const Photo = Module.photo;
+const PhotoComment = Module.photoComment;
+
+router.use(express.json());
+
+router.post('/photo/new', auth, (req, res) => {
+    const photo = new Photo({
+        authorId: req.userData.userId,
+        description: req.body.description,
+        data: req.body.photoString, //base64 encoded
+        likes: [],
+        dislikes: []
+    })
+    photo.save()
+    .then(result => {
+        res.sendStatus(201); //created
+    })
+    .catch(err => {
+        console.log(err);
+        res.json({error: err})
+    });
+})
+
+//get limited, newest photos from all users
+router.post('/photos/public/limited', auth, (req, res) => {
+    let limit = req.body.limit;
+    Photo.find().sort({ createdAt: -1 }).limit(limit)
+        .exec()
+        .then(photos => {
+            return res.status(200).json({
+                photos: photos
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.json({
+                message: 'photos not found',
+                error: err
+            });
+        });
+})
+
+//sends newest, limited photos of a specific user
+router.post('/photos/user/limited', auth, (req, res) => {
+    let limit = req.body.limit;
+    let author = req.body.authorId
+    if(req.body.authorId === "self"){
+        author = req.userData.userId
+    }
+    Photo.find({authorId: author}).sort({ createdAt: -1 }).limit(limit)
+        .exec()
+        .then(photos => {
+            return res.status(200).json({
+                photos: photos
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            return res.json({
+                message: 'photos not found',
+                error: err
+            });
+        });
+})
+
+router.delete('/photo/delete', auth, (req, res) => {
+    Photo.findOneAndDelete({authorId: req.userData.userId})
+        .then(response => {
+            if(response){
+                console.log('photo deleted');
+                res.status(200);
+            }
+            else{
+                console.log('photo deletion failed, photo was not found');
+                res.status(404);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500)
+        });
+})
+
+router.post('/photo/addComment', auth, (req, res) => {
+    Photo.findById(req.body.photoId, (err, photo) => {
+        if(err) console.log(err)
+        else{
+            let comments = photo.comments;
+            console.log('previous comments: ', comments)
+            const photoComment = new PhotoComment({
+                authorId: req.userData.userId,
+                content: req.body.content
+            })
+            comments.push(photoComment);
+            photo.comments = comments;
+            photo.save().then(resp => {
+                console.log("response from saving photo comment", resp);
+                // res.json({
+                //     photo: resp
+                // })
+            })
+        }
+    })
+})
+
+
+module.exports = router;
