9afa9be95d03f3c2a4ba9624e1cf30f27fafe53f
Mikołaj Strusiński <mikistrus@gmail.com>
2021-07-23 00:22:31 +0200

    [ADDED] styling for chat icon for every friend in friendsList

--- frontEnd/src/containers/FriendsList/friendsListItem.js
+++ frontEnd/src/containers/FriendsList/friendsListItem.js
@@ -5,21 +5,25 @@ import { BiMessageDetail } from 'react-icons/bi';
 
 const friendsListItem = (props) => {
     return (
-        <a href={"/user/profile/?id="+props.id} key={props.index} className={classes.containerLink}>
-            <div className={classes.listElement}>
-                <div className={classes.smallFaceContainer}>
-                    <img src={props.photo} alt="friend's face"/>
-                </div>
-                <div className={classes.namesContainer}>
-                    <h1>{props.name} {props.surname}</h1>
-                    <p>@{props.nickname}</p>
+        <>
+            <div className={classes.friendContainer}>
+                <div className={classes.listElement}>
+                    <div className={classes.smallFaceContainer}>
+                        <img src={props.photo} alt="friend's face"/>
+                    </div>
+                    <div className={classes.namesContainer}>
+                        <a href={"/user/profile/?id="+props.id} key={props.index} className={classes.containerLink}>
+                            <h1>{props.name} {props.surname}</h1>
+                        </a>
+                        <p>@{props.nickname}</p>
+                    </div>
                 </div>
                 <div className={classes.chatIcon}>
                     <BiMessageDetail size="2em" color="#0a42a4" />
                 </div>
             </div>
             <hr/>
-        </a>
+        </>
     );
 }
  
