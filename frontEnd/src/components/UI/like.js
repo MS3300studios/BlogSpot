cbad696f1d2f88c1f670db6f9de4650d1e5fa2ca
Mikołaj Strusiński <mikistrus@gmail.com>
2021-08-14 17:41:49 +0200

    [ADDED] logic that determines wether a user is deleting or adding like (in order to prevent sending notifications about deleting)

--- frontEnd/src/components/UI/like.js
+++ frontEnd/src/components/UI/like.js
@@ -20,12 +20,15 @@ class Like extends Component {
         this.process.bind(this);
     }
 
-    process = (arg) => {
+    process = (arg, isDeleting) => {
+        let data = this.state.data;
+        data.isDeleting = isDeleting;
+
         axios({
             method: 'post',
             url: `http://localhost:3001/notifications/create`,
             headers: {'Authorization': this.state.token},
-            data: this.state.data
+            data: data
         })
         .then((res)=>{
             if(res.status===200 || res.status===201){
@@ -45,25 +48,25 @@ class Like extends Component {
             content = <AiFillDislike 
             size={this.props.size} 
             color={this.props.color} 
-            onClick={() => this.process(false)}/>;
+            onClick={() => this.process(false, true)}/>;
         }
         else if(this.props.dislike && !this.props.fill){
             content = <AiOutlineDislike 
             size={this.props.size} 
             color={this.props.color} 
-            onClick={() => this.process(false)}/>;
+            onClick={() => this.process(false, false)}/>;
         }
         else if(this.props.fill){
             content = <AiFillLike 
             size={this.props.size} 
             color={this.props.color} 
-            onClick={() => this.process(true)}/>;
+            onClick={() => this.process(true, true)}/>;
         }
         else if(!this.props.fill){
             content = <AiOutlineLike 
             size={this.props.size} 
             color={this.props.color} 
-            onClick={() => this.process(true)}/>;
+            onClick={() => this.process(true, false)}/>;
         }
 
         let classNames = [classes.likeP];
