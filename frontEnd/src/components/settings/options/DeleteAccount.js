62011df8b10cbfaea5ede7b7a22bdcfdef8af756
Mikołaj Strusiński <mikistrus@gmail.com>
2021-08-20 21:52:58 +0200

    [ADDED] redirecting to socialBoard after user clicks abort in deleteAccount component

--- frontEnd/src/components/settings/options/DeleteAccount.js
+++ frontEnd/src/components/settings/options/DeleteAccount.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import classes from './deleteAccount.module.css';
 import Button from '../../UI/button';
 import axios from 'axios';
 import getToken from '../../../getToken';
 import logout from '../../../logout';
+import { Redirect } from 'react-router-dom';
 
 const DeleteAccount = () => {
+    const [redirectMain, setredirectMain] = useState(false)
+
     const sendRequest = () => {
         const token = getToken();
         axios({
@@ -32,9 +35,10 @@ const DeleteAccount = () => {
                 <h3>This process is irreversable, when you click delete, your account and all of the content related to it will be deleted.</h3>
             </div>
             <div className={classes.buttonsContainer}>
-                <Button >Abort</Button>
+                <Button clicked={()=>setredirectMain(true)}>Abort</Button>
                 <Button btnType="Cancel" clicked={sendRequest}>Delete</Button>
             </div>
+            {redirectMain ? <Redirect to="/" /> : null}
         </div>
     );
 }
