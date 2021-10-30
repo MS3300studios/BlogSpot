6ccb7c8d72a25bc5d841e5a8ed2cc55a260ad14f
Mikołaj Strusiński <mikistrus@gmail.com>
2021-10-30 12:55:38 +0200

    [FIXED] using main uri in online icon

--- frontEnd/src/components/UI/onlineIcon.js
+++ frontEnd/src/components/UI/onlineIcon.js
@@ -1,44 +1,16 @@
-// import React, {useEffect, useState} from 'react';
 import React, { Component } from 'react';
 import io from 'socket.io-client';
+import { MAIN_URI } from '../../config';
 
 import classes from './onlineIcon.module.css';
-
-/*const OnlineIcon = (props) => {
-    const [status, setStatus] = useState(false)
-    const socket = io('http://localhost:3001');
-
-    useEffect(() => {
-        socket.emit('checkUserOnlineStatus', props.online);
-        socket.on('userOnlineStatus', isOnline => {
-            setStatus(isOnline);
-        })
-    }, [props.online, socket]);
-
-    let classname = [classes.onlineIcon].join(" "); 
-
-    status ? classname = [classes.onlineIcon, classes.online].join(" ") 
-        : classname = [classes.onlineIcon, classes.offline].join(" ")
-
-    return (
-        <>
-        {
-            props.hide ? null :
-            <div className={classname} />
-        }
-        </>
-    );
-}
  
-export default OnlineIcon; */
-
 class OnlineIcon extends Component {
     constructor(props) {
         super(props);
         this.state = {
             status: false
         }
-        this.socket = io('http://localhost:3001');
+        this.socket = io(MAIN_URI);
     }
 
     componentDidMount(){
@@ -46,10 +18,6 @@ class OnlineIcon extends Component {
         this.socket.on('userOnlineStatus', isOnline => {
             this.setState({status: isOnline});
         })
-    }
-
-    componentWillUnmount(){
-        
     }
 
     render() { 
@@ -60,10 +28,10 @@ class OnlineIcon extends Component {
 
         return (
             <>
-            {
-                this.props.hide ? null :
-                <div className={classname} />
-            }
+                {
+                    this.props.hide ? null :
+                    <div className={classname} />
+                }
             </>
         );
     }
