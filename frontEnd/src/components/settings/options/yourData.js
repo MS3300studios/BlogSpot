f55beef8d711affaaad3b1ff68028198c185f932
Mikołaj Strusiński <mikistrus@gmail.com>
2021-11-09 15:55:42 +0100

    [UPDATED] message in your data in settings, and improved the styles

--- frontEnd/src/components/settings/options/yourData.js
+++ frontEnd/src/components/settings/options/yourData.js
@@ -5,7 +5,9 @@ import classes from './yourData.module.css';
 const YourData = () => {
     return (
         <div className={classes.yourDataContainer}>
-            <h1>your data</h1>
+            <h1 style={{display: "flex", justifyContent: "center", color: "white", fontSize: "18px", fontWeight: "550"}}>
+                This option hasn't yet been developed. If you want to manage your data, i.e. delete or edit activity, you can go to menu, and press "my activity" button                
+            </h1>
         </div>
     );
 }
